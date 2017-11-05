cbeba0cb7b7f54335d2093949f4663cf735649ab
deni1688 <denisj1688@gmail.com>
2017-11-06 00:51:57 +0100

    Version 1.0.0alpha keys update

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV === 'production'){
-    module.export = require('./keys_prod');
-}else{
-    module.export = require('./keys_dev');
-}
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: porcess.env.GOOGLE_CLIENT_SECRET
+};

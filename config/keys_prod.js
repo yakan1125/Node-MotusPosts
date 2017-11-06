1a8271af7c69ca0d08cf036421507770a78b6fad
deni1688 <denisj1688@gmail.com>
2017-11-06 01:19:25 +0100

    Version 1.0.0alpha keys update 2

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: porcess.env.GOOGLE_CLIENT_SECRET
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
